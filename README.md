b3aa9829e1281dfb31462c96db254da559c9e946
Kevin Griffin <griffin.kev@gmail.com>
2021-07-19 14:19:57 -0400

    initial GLEIF schema
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 
 The vLEI is a open standard type of W3C Verifiable Credential (VC) that leverages the W3C Decentralized Identifier (DID) standard. The DID method is `did:keri` which is based onthe [KERI](https://keri.one) identifier system .
 
+### Development
+
+To verify Schema match the samples run:
+
+```shell
+pytest
+```
+
+To generate new SAIDs for updates schema run:
+
+```shell
+python3 src/vlei/generate_said.py
+```
+
---
+++ src/vlei/generate_said.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+"""
+vLEI module
+
+"""
+import json
+import os
+from pathlib import Path
+
+from keri.core import scheming
+
+
+def main():
+    path = Path(__file__).parent
+
+    schemaCache = cache(f'{path}/../../schema/', dict())
+
+    for f, d in schemaCache.items():
+        with open(f, 'w') as f:
+            f.write(json.dumps(scheming.Schemer(sed=d).sed, indent=4, sort_keys=True))
+
+
+def cache(path, d):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.json'):
+                with open(os.path.join(root, file), 'r') as f:
+                    d[os.path.join(root, file)] = json.load(f)
+
+    return d
+
+
+if __name__ == "__main__":
+    main()
