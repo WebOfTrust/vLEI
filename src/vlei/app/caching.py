8dc527925d6976ab95f1e4aaa5829b27515ce12f
pfeairheller <pfeairheller@gmail.com>
2022-06-02 11:23:46 -0700

    Update to schema for vLEI and ViRA go live.
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- src/vlei/app/caching.py
+++ src/vlei/app/caching.py
@@ -9,7 +9,7 @@ import os
 from keri.core import scheming
 
 
-def cache(path, d):
+def cacheSchema(path, d):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.json'):
@@ -18,6 +18,18 @@ def cache(path, d):
                     schemer = scheming.Schemer(sed=ked)
                     print(f"caching schema {schemer.said}")
                     d[schemer.said] = schemer.raw
+
+    return d
+
+
+def cacheCredential(path, d):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('-acdc.cesr'):
+                with open(os.path.join(root, file), 'r') as f:
+                    said = file.rstrip('-acdc.cesr')
+                    print(f"caching credential {said}")
+                    d[said] = f.read()
 
     return d
 
--- src/vlei/app/serving.py
+++ src/vlei/app/serving.py
@@ -3,34 +3,41 @@
 vLEI.serving module
 
 """
+
 import falcon
+from hio.core import http
 
 from vlei.app import caching
 
 
 class SchemaEnd:
 
-    def __init__(self, directory):
-        self.schemaCache = caching.cache(directory, dict())
+    def __init__(self, schemaDir, credDir):
+        self.schemaCache = caching.cacheSchema(schemaDir, dict())
+        self.credentialCache = caching.cacheCredential(credDir, dict())
 
     def on_get(self, _, rep, said):
-        if said not in self.schemaCache:
-            rep.status = falcon.HTTP_404
-            rep.text = f"Schema {said} not found"
-            return None
+        if said in self.schemaCache:
+            data = self.schemaCache[said]
 
-        data = self.schemaCache[said]
+            rep.status = falcon.HTTP_200
+            rep.content_type = "application/schema+json"
+            rep.data = data
+            return
 
-        rep.status = falcon.HTTP_200
-        rep.content_type = "application/schema+json"
-        rep.data = data
+        if said in self.credentialCache:
+            data = self.credentialCache[said]
+
+            rep.status = falcon.HTTP_200
+            rep.content_type = "application/acdc+json"
+            rep.data = data.encode("utf-8")
+            return
 
 
-def loadEnds(app, directory):
+def loadEnds(app, schemaDir, credDir):
 
-    schemaEnd = SchemaEnd(directory=directory)
+    sink = http.serving.StaticSink(staticDirPath="./static")
+    app.add_sink(sink, prefix=sink.DefaultStaticSinkBasePath)
+
+    schemaEnd = SchemaEnd(schemaDir=schemaDir, credDir=credDir)
     app.add_route("/oobi/{said}", schemaEnd)
-
-
-
-
