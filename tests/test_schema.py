b935e7ad7ec6c152e26666667937b4f19ee4536a
Kevin Griffin <griffin.kev@gmail.com>
2023-02-01 12:13:01 -0500

    adds tests for #73 (#74)
    * adds tests for #73

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 import pytest
 from keri.core import scheming, coring
+from keri.kering import ValidationError
 
 
 @pytest.mark.parametrize("filename", ["verifiable-ixbrl-report-attestation.json",
-                                      "legal-entity-engagement-context-role-vLEI-credential.json",
                                       "legal-entity-official-organizational-role-vLEI-credential.json",
                                       "legal-entity-vLEI-credential.json",
                                       "qualified-vLEI-issuer-vLEI-credential.json",
@@ -19,6 +19,40 @@ def test_schema_example(fmt, filename):
         schemer = scheming.Schemer(sed=json.load(schema))
 
         schemer.verify(sample.read().encode("utf-8"))
+
+
+@pytest.mark.parametrize("sample", [
+    "legal-entity-engagement-context-role-vLEI-credential-auth.json",
+    "legal-entity-engagement-context-role-vLEI-credential-le.json",
+    "legal-entity-engagement-context-role-vLEI-credential-said.json",
+])
+@pytest.mark.parametrize("schema", ["legal-entity-engagement-context-role-vLEI-credential.json", ])
+@pytest.mark.parametrize("fmt", ["acdc"])
+def test_ecr_schema_with_good(fmt, schema, sample):
+    with open(f'{Path(__file__).parent}/../schema/{fmt}/{schema}', 'r') as schma, \
+            open(f'{Path(__file__).parent}/../samples/acdc/{sample}', 'r') as smple:
+        schemer = scheming.Schemer(sed=json.load(schma))
+
+        assert schemer.verify(smple.read().encode("utf-8"))
+
+
+@pytest.mark.parametrize("sample", [
+    "legal-entity-engagement-context-role-vLEI-credential-bad.json",
+    "legal-entity-engagement-context-role-vLEI-credential-auth-bad.json",
+    "legal-entity-engagement-context-role-vLEI-credential-le-bad.json",
+    "legal-entity-engagement-context-role-vLEI-credential-missing-bad.json",
+])
+@pytest.mark.parametrize("schema", ["legal-entity-engagement-context-role-vLEI-credential.json", ])
+@pytest.mark.parametrize("fmt", ["acdc"])
+def test_ecr_schema_with_bad(fmt, schema, sample):
+    with open(f'{Path(__file__).parent}/../schema/{fmt}/{schema}', 'r') as schma, \
+            open(f'{Path(__file__).parent}/../test-vectors/{fmt}/ecr-failures/{sample}', 'r') as smple:
+        schemer = scheming.Schemer(sed=json.load(schma))
+
+        with pytest.raises(ValidationError) as e:
+            schemer.verify(smple.read().encode("utf-8"))
+
+        assert e.type is ValidationError
 
 
 def test_legal_entity_chain():
